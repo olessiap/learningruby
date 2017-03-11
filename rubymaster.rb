1e168bdaebdb9d84b24d6d146632e966d9f2350e
Olessia Potapova <olessia.potapova@gmail.com>
2017-03-10 19:08:02 -0800

    latest

--- rubymaster.rb
+++ rubymaster.rb
@@ -1,1 +1,28 @@
+###STRING METHODS###
+
 "olessia potapova".length
+"olessia potapova".reverse #good for sorting from highest/lowest
+puts "olessia".upcase # OLESSIA
+puts "POtapova".downcase # potapova
+
+name = "Olessia"
+name.downcase.reverse.upcase #AISSELO
+
+print "whats your last name?"
+last_name = gets.chomp # "gets" is input "chomp" removes extra ruby line
+last_name2 = last_name.capitalize
+last_name.capitalize!
+
+puts "your last name is #{last_name}!"
+
+###IF/ELSE
+
+if 5 < 7
+  puts "7 is less than 5"
+elsif 5 > 7
+  puts "5 is greater than 7"
+else
+  puts "5 is 7"
+end
+
+puts "dr"
