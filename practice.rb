bb5ba6577526151238682dab01e0fc4a0fef5950
Olessia Potapova <olessia.potapova@gmail.com>
2017-04-12 20:40:35 +0300

    more practice

--- practice.rb
+++ practice.rb
@@ -6,7 +6,7 @@ puts (60 * (24 * 365)) * 60 * 26  #my age in seconds
 
 # Write a program which asks for a person's first name, then middle, then last.
 #Finally, it should greet the person using their full name.
-
+=begin
 puts 'What is your first name? '
 first_name = gets.chomp
 
@@ -25,4 +25,36 @@ puts 'Hello ' + first_name + ' ' + middle_name + ' ' + last_name + '. It\'s so g
 puts 'Whats your favorite number hun?'
 favorite_number = gets.chomp.to_i
 new_number = favorite_number + 1
-puts 'I think ' + new_number.to_s + ' is much better ;)' # need to convert new_number to string 
+puts 'I think ' + new_number.to_s + ' is much better ;)' # need to convert new_number to string
+=end
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Ask for first, middle, and last names individually, and then adds those lengths together
+
+puts "first name: "
+first_name = gets.chomp
+
+puts 'middle name: '
+middle_name = gets.chomp
+
+puts 'last name: '
+last_name = gets.chomp
+
+full_name_spaces = first_name + ' ' + middle_name + ' ' + last_name + ' '
+
+full_name_letters = first_name + middle_name + last_name
+
+puts 'your full name is ' + full_name_spaces + '!'
+
+puts 'Your full name is ' + full_name_letters.length.to_s + ' letters long'
