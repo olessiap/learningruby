ce50eed54f57aa56ffe65244412d4179dda9384f
Olessia Potapova <olessia.potapova@gmail.com>
2017-04-25 11:45:02 +0300

    renamed some files to organize better

---
+++ practice2.rb
@@ -0,0 +1,33 @@
+#1
+#print out 1-10
+
+#2
+#exercise: print "Ruby!" string 30 times using loop
+
+#3
+#exercise: print 1-5 using while
+
+#4
+#write a method that returns a string, adding to that string a person's name.
+
+#5
+#Century from year
+#Given a year, return the century it is in.
+#The first century spans from the year 1 up to and including the year 100,
+#the second - from the year 101 up to and including the year 200, etc.
+
+#6
+#Palindrome
+#Given the string, check if it is a palindrome.
+
+#7
+#Given an array of integers,
+#find the pair of adjacent elements that has the largest product
+#and return that product.
+
+#8
+#evaluate the expression 12 ** 2, and print the result 3 times
+
+number = 12 ** 2
+
+puts number.3
